54e0a774571baa2288a384f42b38939c7d46231c
tianxin <tianxin04@baidu.com>
2021-10-19 20:42:13 +0800

    add inference tool demo

--- script/inference_de.sh
+++ script/inference_de.sh
@@ -13,11 +13,11 @@ PYTHON_BIN="/usr/local/bin/python3.7"
 test_data="./test_data"
 
 unset CUDA_VISIBLE_DEVICES
-${PYTHON_BIN} -u -m paddle.distributed.launch --gpus "0" \
+${PYTHON_BIN} -u -m paddle.distributed.launch --gpus "7" \
     inference_de.py \
     --device gpu \
     --params_path "./model_13330/model_state.pdparams" \
     --batch_size 128 \
     --max_seq_length 128 \
-    --text_file ${test_data} \
+    --text_file "./demo_data/test_data" \
     --output_file "para_embeddings"
